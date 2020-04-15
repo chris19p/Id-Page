096cac9c7cc306cd3533cb7d46c50c22afcf2b3c
chris19p <61362187+chris19p@users.noreply.github.com>
2020-04-15 11:07:34 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+function idCard () {
+    var firstName = document.getElementById("firstName").value;
+    var lastName = document.getElementById("lastName").value;
+    var address = document.getElementById("address").value;
+    
+    var fullName = firstName + " " + lastName;
+    
+    document.getElementById("postFullName").innerHTML = fullName;
+    document.getElementById("postAddress").innerHTML = "Address: " + address;
+    
+    var age = document.getElementById("age").value;
+    var phoneNumber = document.getElementById("phoneNumber").value;
+    
+    var numberArray = [];
+    numberArray.push(age,phoneNumber);
+    
+    for (var i = 0; i < numberArray.length; i++){
+      if (numberArray[i]<100){
+        document.getElementById("postAge").innerHTML="Age: " + age;
+      } else if(numberArray[i]>=100){
+        document.getElementById("postPhoneNumber").innerHTML = "Phone Number: " + phoneNumber;
+      }
+      
+    }
+  }
